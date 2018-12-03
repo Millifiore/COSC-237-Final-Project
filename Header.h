83c4828dd55eb327706666e5bb860aefaaea7fea
Kayla Thompson <kthompson1102@gmail.com>
2018-12-03 16:44:37 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,2 +1,29 @@
-#pragma once
+#ifndef Calendar_H
+#define Calendar_H
+class Calendar {
+    private:
+        string event;
+        int date;
+        int time;
+        string location;
+    public:
+        Calendar();
+        void setEvent(string);
+        void setDate(int);
+        void setTime(int);
+        void setLocation(string);
+    
+        string getEvent() const
+            { return event;}
+        int getDate() const
+            { return date;}
+        int getTime() const
+            { return time;}
+        string getLocation() const
+            { return location;}
+    
+};
+
+#endif
+
 
