bc123b5e96a805a6a1504f5a80a06eddf37cf318
kthompson1102 <43994620+kthompson1102@users.noreply.github.com>
2018-12-03 17:08:08 -0500

    Update Calendar.h

--- Calendar.h
+++ Calendar.h
@@ -4,20 +4,20 @@ class Calendar {
     private:
         string event;
         int date;
-        int time;
+        float time;
         string location;
     public:
         Calendar();
         void setEvent(string);
         void setDate(int);
-        void setTime(int);
+        void setTime(float);
         void setLocation(string);
     
         string getEvent() const
             { return event;}
         int getDate() const
             { return date;}
-        int getTime() const
+        float getTime() const
             { return time;}
         string getLocation() const
             { return location;}
