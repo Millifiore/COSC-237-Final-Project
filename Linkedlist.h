44939d0ce16eb547256b934dd266220378bae14d
Xavier Ray <xaray1@morgan.edu>
2018-12-03 17:47:45 -0500

    Update Linkedlist.h

--- Linkedlist.h
+++ Linkedlist.h
@@ -4,18 +4,26 @@
 class MonthList {
 	private:
 		struct ListNode {
-			double value;
+			string Month;
+			string *Days;
+			ListNode(const int D) {
+				string arr[D];
+				Days = arr;
+			}
 			struct ListNode *next;
 		};
 
 		ListNode *head;
+		int Year;
 
 	public:
+
 		MonthList()
 		{head = nullptr;}
 
 		void displayMonths() const;
-		// void importData() 
+		void appendMonth(string,const int D)
+		void CreateYear() const:
+		
 };
-
 #endif
