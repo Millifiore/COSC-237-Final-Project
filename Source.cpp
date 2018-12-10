4cfc4267f3bc1ed11d531c4af00124e64ea1810f
Kayla Thompson <kthompson1102@gmail.com>
2018-12-09 19:24:31 -0500

    k

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,19 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+#include "Calendar.h"
 using namespace std;
 
 int main(){
-    cout << "Hello world" << endl;
+    fstream dataFile;
+    dataFile.open("Calendar_Events.txt", ios::in);
+    string e, l;
+    int d;
+    float t;
+    if (dataFile.is_open())
+        {
+            
+        }
     return 0;
 }
 
