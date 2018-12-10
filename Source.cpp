754a4cd0389a81af2c4a0ed01aefb8b9907aab41
Kayla Thompson <kthompson1102@gmail.com>
2018-12-10 18:16:30 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,7 @@ int main(){
     fstream dataFile;
     dataFile.open("Calendar_Events.txt", ios::in);
     string e, d, t, l;
-    string event[4]
+    string event[8]
     if (!dataFile)
         {
             cout << "Couldn't open the file" << endl;
@@ -22,6 +22,20 @@ int main(){
              i++;
             }
         }
+    dataFile.close();
+    Calendar event1;
+    event1.setEvent(event[0]);
+    event1.setDate(event[1]);
+    event1.setTime(event[2]);
+    event1.setLocation(event[3]);
+    
+    Calendar event2;
+    event2.setEvent(event[4]);
+    event2.setDate(event[5]);
+    event2.setTime(event[6]);
+    event2.setLocation(event[7]);
+    
+    
     return 0;
 }
 
